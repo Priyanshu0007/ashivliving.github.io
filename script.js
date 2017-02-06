aecbdb834e13f0f9a77dd483333ef02c3b7902e5
ashivliving <ashivgupta23@gmail.com>
2017-02-06 16:03:54 +0530

    Map added

---
+++ script.js
@@ -0,0 +1,49 @@
+function initMap(as,ae,bs,be) {
+    var aStart = as,aEnd = ae,bStart = bs,bEnd = be;
+    var pointA = new google.maps.LatLng(aStart, aEnd),
+        pointB = new google.maps.LatLng(bStart, bEnd),
+        myOptions = {
+            zoom: 17,
+            center: pointA
+        },
+        map = new google.maps.Map(document.getElementById('map-canvas'), myOptions),
+        // Instantiate a directions service.
+        directionsService = new google.maps.DirectionsService,
+        directionsDisplay = new google.maps.DirectionsRenderer({
+            map: map
+        }),
+        markerA = new google.maps.Marker({
+            position: pointA,
+            title: "point A",
+            label: "A",
+            map: map
+        }),
+        markerB = new google.maps.Marker({
+            position: pointB,
+            title: "point B",
+            label: "B",
+            map: map
+        });
+
+    // get route from A to B
+    calculateAndDisplayRoute(directionsService, directionsDisplay, pointA, pointB);
+
+}
+
+
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay, pointA, pointB) {
+    directionsService.route({
+        origin: pointA,
+        destination: pointB,
+        avoidTolls: true,
+        avoidHighways: false,
+        travelMode: google.maps.TravelMode.DRIVING
+    }, function (response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+        } else {
+            window.alert('Directions request failed due to ' + status);
+        }
+    });
+}
